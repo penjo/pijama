ed5209cc2f59d6d5f5e15308042c22676e13ef91
penjo <97688426+penjo@users.noreply.github.com>
2023-02-23 20:40:06 +0700

    update

--- CODE/PijamaWeb/src/components/footer/Footer.jsx
+++ CODE/PijamaWeb/src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import ChevronRight from "../../assets/images/Chevron right white.png";
 import Logo from "../../assets/images/LOGO.png";
+import {BiPhone} from "react-icons/bi";
 import Momo from "../../assets/images/image 10.png";
 import VNPay from "../../assets/images/image 11.png";
 import Cod from "../../assets/images/Vector.png";
@@ -12,7 +13,7 @@ import Tik from "../../assets/images/Vector3.png";
 const Footer = () => {
 	return (
 		<div className="footer">
-			<div className="title">cooperation</div>
+			{/* <div className="title">cooperation</div>
 			<div className="phone">(+84) 123 456 789</div>
 			<div className="mail">luladesignbyla@gmail.com</div>
 			<div className="link">
@@ -45,8 +46,42 @@ const Footer = () => {
 				<img src={FB} alt="" />
 				<img src={Ins} alt="" />
 				<img src={Tik} alt="" />
+			</div> */}
+			 <div className='footer_logo'>
+			 <img src={Logo} className="logo-img" alt="logo" />
+        	</div>
+			<div className="footer_block1">
+				<p className="phone">HotLine: <a href="tel:+84 123 456 789" >(+84) 123 456 789</a></p>
+				<p className="mail">Email: <a href="mailto:luladesignbyla@gmail.com">luladesignbyla@gmail.com</a></p>	
 			</div>
-		</div>
+			<div className="row">
+			<div className="footer_block2">
+				<h3 className="footer_title">Hỗ Trợ Khách Hàng</h3>
+					<ul>
+						<li>Giới thiệu về LULA</li>
+						<li>Hướng dẫn thanh toán</li>
+						<li>Hướng dẫn mua hàng</li>
+						<li>Chính sách bảo mật</li>
+						<li>Tuyển dụng</li>
+					</ul>
+			</div>
+			<div className="footer_block3">
+				<h3 className="footer_title">Hỗ Trợ Khách Hàng</h3>
+					<ul>
+						<li>Giới thiệu về LULA</li>
+						<li>Hướng dẫn thanh toán</li>
+						<li>Hướng dẫn mua hàng</li>
+						<li>Chính sách bảo mật</li>
+						<li>Tuyển dụng</li>
+					</ul>
+			</div>
+			</div>
+			<div className="social">
+				<img src={FB} alt="" />
+				<img src={Ins} alt="" />
+				<img src={Tik} alt="" />
+			</div> 
+			</div>
 	);
 };
 
--- CODE/PijamaWeb/src/components/header/Logo.js
+++ CODE/PijamaWeb/src/components/header/Logo.js
@@ -4,7 +4,10 @@ import Logo from "../../assets/images/LOGO.png";
 
 const HeaderLogo = () =>{
     return(
+        <>
         <img src={Logo} className="logo-img" alt="logo" />
+        <h3>LULA SHOP</h3>
+        </>
     )
 }
 export default HeaderLogo;
